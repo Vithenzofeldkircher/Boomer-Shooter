62e61f91f4f51baa584102d9ffff7d1888912db0
Vithenzofeldkircher <vithenzo.feldkircher@alunos.sc.senac.br>
2026-04-30 09:29:10 -0300

    Update

--- Assets/Scripts/Scripts_Vithenzo/Kit_Player_Controllers.cs
+++ Assets/Scripts/Scripts_Vithenzo/Kit_Player_Controllers.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Kit_Player_Controllers : MonoBehaviour
 {
+    public float Velociade_Do_Player = 5.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
