ac7977d1f0ebcf1d46bf88446bfd7f291a0efbdd
GabrielDjones <gabriel.farias@alunos.sc.senac.br>
2026-04-30 15:48:29 -0300

    enemy improves

--- Assets/Djones/Scripts/Enemy/walkEnemy.cs
+++ Assets/Djones/Scripts/Enemy/walkEnemy.cs
@@ -1,22 +1,41 @@
-using StarterAssets;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.SceneManagement;
 
 public class walkEnemy : MonoBehaviour
 {
     [SerializeField] private float _visionDistance = 3;
+    [SerializeField] private Transform[] _pathPoints;
+    private int _pathIndex = 0;
     private NavMeshAgent _agent;
+    private bool _chasing;
+
 
     void Start()
     {
-       _agent = GetComponent<NavMeshAgent>();    
+        _agent = GetComponent<NavMeshAgent>();
+        Debug.Log("going");
+        _agent.SetDestination(_pathPoints[_pathIndex].position);
     }
 
     void Update()
     {
         {
+            if (!_agent.pathPending && _agent.remainingDistance < 0.5f  &&!_chasing)
+            {
+                Debug.Log("start");
+                _pathIndex++;
+                if (_pathIndex < _pathPoints.Length)
+                {
+                    Debug.Log("going");
+                    _agent.SetDestination(_pathPoints[_pathIndex].position);
+                }
+                else
+                {
+                    Debug.Log("reset");
+                    _pathIndex = 0;
+                }
+            }
+            _chasing = false;
             Ray ray = new Ray(gameObject.transform.position,gameObject.transform.forward);
             RaycastHit hit;
 
@@ -24,9 +43,10 @@ public class walkEnemy : MonoBehaviour
                 return;
             if (!hit.collider.CompareTag("Player"))
                 return;
-                _agent.SetDestination(hit.transform.position);
+            _chasing = true;
+            _agent.SetDestination(hit.transform.position);
             //_agent.remainingDistance 
-            gameObject.transform.rotation = Quaternion.LookRotation(gameObject.transform.position, hit.transform.position).normalized ;
+            gameObject.transform.LookAt(hit.transform.position); 
         }
     }
 }
